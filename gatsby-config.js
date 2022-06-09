3da4aa3c2b58eb6a9ff7e6385423b6043c07240b
BAUD Vincent <VBD2@dsone.3ds.com>
2022-06-09 09:57:29 +0900

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
+    title: "My Website",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+  ],
 }
---
+++ src/pages/blog.js
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { graphql } from 'gatsby'
+const BlogPage = ({ data }) => {
+    return (
+        <Layout pageTitle="My Blog Posts">
+            <ul>
+                {
+                    data.allMdx.nodes.map((node) => (
+                        <article key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </article>
+                    ))
+                }
+            </ul>
+        </Layout>
+    )
+}
+export const query = graphql`
+query {
+  allMdx(sort: {order: DESC, fields: frontmatter___date}) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+      }
+      id
+      body
+    }
+  }
+}
+`
+export default BlogPage
